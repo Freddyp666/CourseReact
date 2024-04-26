3841f01387ae5f89881aaad4dbe8d1f279b890e2
Fredy Patricio Pallo Cuzco <52755277+Freddyp666@users.noreply.github.com>
2024-04-26 00:34:16 -0500

    Proyecto 1 Terminado

--- Proyectos/Mi App-Testimonio/testimonios-pagina/src/App.js
+++ Proyectos/Mi App-Testimonio/testimonios-pagina/src/App.js
@@ -1,11 +1,34 @@
-import Testimonio from './componentes/Testimonio';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
+
 
 function App() {
   return (
     <div className="App">
-    <Testimonio />
+      <div className="contenedor-principal">
+        <h1>Los tres mas fuertes</h1>
+        <Testimonio 
+          nombre='Monkey D. Luffy'
+          imagen='luffy'
+          pais='East Blue'
+          cargo='Capitán'
+          testimonio='¡Voy a convertirme en el Rey de los Piratas! ¡Lo juro por mi propio orgullo y por los sueños de mis amigos! ¡Nunca retrocederé ni me rendiré hasta que haya alcanzado el One Piece y haya conquistado los mares!' />
+        
+        <Testimonio
+          nombre='Portgas D. Ace'
+          imagen='ace'
+          pais='East Blue'
+          cargo='Comandante'
+          testimonio='Voy a vivir libre y sin arrepentimientos. Defenderé a mis amigos y seguiré mi propio camino, desafiando cualquier obstáculo con pasión y determinación. Mi fuego arderá siempre brillante, iluminando el camino hacia un futuro mejor para todos.' />
 
+        <Testimonio
+          nombre='Jean-Philippe Sabo'
+          imagen='sabo'
+          pais='East Blue'
+          cargo='Emperador'
+          testimonio='Lucharé por la libertad y la justicia, protegiendo a mis seres queridos y luchando contra la opresión. Siempre seguiré mi camino con valentía y determinación, buscando un mundo donde todos puedan vivir en paz y felicidad.' />
+
+      </div>
     </div>
   );
 }
--- Proyectos/Mi App-Testimonio/testimonios-pagina/src/componentes/Testimonio.jsx
+++ Proyectos/Mi App-Testimonio/testimonios-pagina/src/componentes/Testimonio.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import '../stylesheet/Testimonio.css'
 
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img
         className='imagen-testimonio'
-        src={require('../imagenes/luffy3.jpg')}
-        alt='Foto de lufy 3' />
-
+        src={require(`../imagenes/vida-${props.imagen}.jpg`)}
+        alt={`Imagen de ${props.nombre}`}
+      />
       <div className="contenedor-text-testimonio">
-        <p className="nombre-testimonio">Monkey D Luffy</p>
-        <p className="cargo-testimonio">Navegador en GrandLine</p>
-        <p className="texto-testimonio">
-          Gear Third es una técnica de combate de Monkey D. Luffy en "One Piece". Consiste en inflar sus huesos para aumentar su tamaño y fuerza. Aunque poderosa, reduce su velocidad y movilidad. Es clave en batallas difíciles.</p>
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> de {props.pais} </p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>RedLine</strong></p>
+        <p className="texto-testimonio">{props.testimonio}</p>
       </div>
     </div>
   );
